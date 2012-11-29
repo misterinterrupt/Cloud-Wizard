6e99c2a67645d1edcd31f2326aabae3b5fdce1f0
Matt Howell <misterinterrupt@gmail.com>
2012-11-29 02:37:31 -0800

    intial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+var http = require('http'),
+    connect = require('connect'),
+    sio = require('socket.io'),
+    RedisStore = require('connect-redis')(connect),
+    parseCookie = require('cookie').parse,
+    cookieSignature = require('cookie-signature'),
+    app = connect(),
+    redisOptions = {
+      'host': 'localhost',
+      'port': '6379',
+      'db': 'cloudwizard',
+      'pass': ''
+    },
+    sessionStore = new RedisStore(redisOptions),
+    sessionConfig = {
+      store: sessionStore,
+      secret: 'sp3480oo34t8assti89c',
+      key: 'cloudwizard.sid'
+    },
+    sc_auth_options = {
+      host: 'www.soundcloud.com',
+      path: '/connection',
+      method: 'POST',
+      headers: {
+        client_id: "2105d18bcf851694947795f67b045675",
+        client_secret: "ec9f9415a6203ee7d31f01b09fd94af9",
+        redirect_uri: "http://matthewdhowell.com/cloudwizard/receiver/soundcloud"
+    //    grant_type: "client_credentials",
+    //    code: "",
+    //    state: ""
+      }
+    };
+
+
+function socketSessionAuthorization(data, accept) {
+  
+  // has a cookie header
+  if(data.headers.cookie) {
+    var cookieDough = parseCookie(data.headers.cookie)[sessionConfig.key].slice(2);
+    data.cookie = cookieSignature.unsign(cookieDough, sessionConfig.secret);
+    data.sessionID = data.cookie;
+    sessionStore.get(data.sessionID, function(err, session) {
+      if(err || !session) {
+        return accept('Error: Session Unavailable.', false);
+      } else {
+        data.session = session;
+        return accept(null, true);
+      }
+    });
+  } else {
+  
+    return accept('Error: Session Unavailable.', false);
+  }
+}
+
+function onSocketConnect(socket) {
+  console.log('a socket connected with sid: ' + socket.handshake.sessionID);
+  socket.on('get tracks', getTracks);
+}
+
+function soundcloudAPIAuth() {
+  
+}
+
+function getTracks() {
+  //http.request(sc_auth_options, )
+  console.log(arguments);
+}
+
+// Bootstrap-ish stuff happens after the store is available
+function onRedisClientConnect() {
+  app.use(connect.static(__dirname + '/public'));
+  app.use(connect.cookieParser());
+  app.use(connect.session(sessionConfig));
+  // app.use(function(req, res, next){
+  //   res.write('Hello from Connect!\n');
+  //   res.end(JSON.stringify(req.cookies));
+  // });
+  var server = http.createServer(app).listen(4444),
+      io = sio.listen(server);
+  io.set('authorization', socketSessionAuthorization);
+  io.sockets.on('connection', onSocketConnect);
+}
+
+
+sessionStore.client.on('connect', onRedisClientConnect);
